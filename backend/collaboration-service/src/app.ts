e7f58fb3dfa2c82a2580f4304b4ff26e86b151b5
c0j0s <c0j0s@outlook.com>
2023-10-11 16:52:32 +0800

    change: update express-pino to pino-http

--- backend/collaboration-service/src/app.ts
+++ backend/collaboration-service/src/app.ts
@@ -4,34 +4,19 @@ import { Socket } from "socket.io";
 import cors, { corsOptions } from "./middleware/cors";
 import { SocketEvent } from "./lib/enums/SocketEvent";
 import { SocketHandler } from "./controllers";
-import expressPino from 'express-pino-logger';
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { LoggerOptions } from "pino";
 import logger from './lib/utils/logger'; 
+import pinoHttp from "pino-http";
 
-
-import pino from 'pino';
-
-// export function createLogger(prefix: string) {
-//     return pino({
-//         level: process.env.LOG_LEVEL || 'debug',
-//         formatters: {
-//             log(object) {
-//                 return { ...object, msg: `[${prefix}] ${object.msg}` };
-//             },
-//         },
-//     });
-// }
-
-// const logger = createLogger('collaboration-service');
 
 dotenv.config();
 
 const app = express();
-const expressLogger = expressPino({ logger });
+const expressLogger = pinoHttp({ logger });
 
 app.use(cors);
+app.use(expressLogger);
 
 const server = createServer(app);
 
@@ -45,7 +30,7 @@ io.on(SocketEvent.CONNECTION, (socket: Socket) => {
 })
 
 server.listen(process.env.SERVICE_PORT, () => {
-  console.log(`Server running on port ${process.env.SERVICE_PORT}`);
+  logger.info(`Server running on port ${process.env.SERVICE_PORT}`);
 });
 
 export { io };
