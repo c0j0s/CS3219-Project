9091884ad4cbaed63aff0bf33d05716f63513361
c0j0s <c0j0s@outlook.com>
2023-10-11 17:01:25 +0800

    fix: fix issue with logger prefix

--- backend/collaboration-service/src/lib/utils/logger.ts
+++ backend/collaboration-service/src/lib/utils/logger.ts
@@ -3,11 +3,7 @@ import pino from 'pino';
 export function createLogger(prefix: string) {
     return pino({
         level: process.env.LOG_LEVEL || 'debug',
-        formatters: {
-            log(object) {
-                return { ...object, msg: `[${prefix}] ${object.msg}` };
-            },
-        },
+        msgPrefix: `[${prefix}] `,
     });
 }
 
--- backend/matching-service/src/lib/utils/logger.ts
+++ backend/matching-service/src/lib/utils/logger.ts
@@ -3,13 +3,9 @@ import pino from 'pino';
 export function createLogger(prefix: string) {
     return pino({
         level: process.env.LOG_LEVEL || 'debug',
-        formatters: {
-            log(object) {
-                return { ...object, msg: `[${prefix}] ${object.msg}` };
-            },
-        },
+        msgPrefix: `[${prefix}] `,
     });
 }
 
 const logger = createLogger('matching-service');
-export default logger;
+export default logger
