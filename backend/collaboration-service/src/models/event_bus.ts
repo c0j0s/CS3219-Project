d2fc881a7bd7d66690d7edb34ebfcd6721c0bc54
Weida Tay <weida.tay99@gmail.com>
2023-10-22 15:05:18 +0800

    successfully integrated matching-collaboration docker containers with docker-compose

--- backend/collaboration-service/src/models/event_bus.ts
+++ backend/collaboration-service/src/models/event_bus.ts
@@ -1,14 +1,24 @@
 import { Redis } from 'ioredis';
 import dotenv from 'dotenv';
+import { create } from 'domain';
 
 dotenv.config();
 
-function getEventBusUrl() {
 
-  if (process.env.REDIS_EVENT_BUS) {
-    return process.env.REDIS_EVENT_BUS;
-  }
-  throw new Error("REDIS_EVENT_BUS not found");
+function createEventBusClient() {
+
+  if (process.env.NODE_ENV === 'test') 
+    return new Redis();
+  
+  // If we are running in a container, use the container name as the host
+  if (process.env.EVENT_BUS_CONTAINER_NAME) 
+    return new Redis({ host: process.env.EVENT_BUS_CONTAINER_NAME });
+  
+  // For local deployment, use the local url
+  if (process.env.REDIS_EVENT_BUS) 
+    return new Redis(process.env.REDIS_EVENT_BUS);
+
+  throw new Error('Event bus environment variables are not defined')
 }
 
-export const eventBus = process.env.NODE_ENV === 'test' ? new Redis : new Redis(getEventBusUrl());
+export const eventBus = createEventBusClient();
