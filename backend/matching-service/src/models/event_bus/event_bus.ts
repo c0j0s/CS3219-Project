1e92b9768a2330c091b320f9d63167e8349fa235
Weida Tay <weida.tay99@gmail.com>
2023-10-22 14:03:41 +0800

    create relevant docker files for subcomponents

--- backend/matching-service/src/models/event_bus/event_bus.ts
+++ backend/matching-service/src/models/event_bus/event_bus.ts
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-function getEventBusUrl() {
-
+// Local redis url for npm run dev
+function getLocalUrl(){
   if (process.env.REDIS_EVENT_BUS) {
-    return process.env.REDIS_EVENT_BUS;
+    return process.env.REDIS_EVENT_BUS
   }
-  throw new Error("REDIS_EVENT_BUS not found");
+  throw new Error('REDIS_EVENT_BUS not defined')
 }
 
-export const eventBus = new Redis(getEventBusUrl());
+export const eventBus = process.env.EVENT_BUS_CONTAINER_NAME 
+                        ? new Redis({ host: process.env.EVENT_BUS_CONTAINER_NAME })
+                        : new Redis(getLocalUrl());
